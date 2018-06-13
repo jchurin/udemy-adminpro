a9753fb87f95461f0c61f5fa5875a32336c1165b
jchurin95 <juanchurin@gmail.com>
2018-06-13 16:26:36 -0300

    observables

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+// import { Observable } from 'rxjs/Observable';
+// tslint:disable-next-line:import-blacklist
+import { Observable } from 'rxjs/Rx';
+import { retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -10,6 +14,31 @@ export class RxjsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.countThreeSeconds()
+    .pipe(
+      retry(2) // Retry obs if fails
+    ).subscribe(
+      response => console.log('Subs', response),
+      error => console.error('Error en el obs', error),
+      () => console.warn('Observer finalizado')
+    );
+  }
+
+  countThreeSeconds(): Observable<number> {
+    return new Observable( observer => {
+      let counter = 0;
+      const interval = setInterval(() => {
+        counter += 1;
+        observer.next(counter);
+        if (counter === 3) {
+          clearInterval(interval);
+          observer.complete();
+        } else if (counter === 2) {
+          // clearInterval(interval);
+          observer.error('ES UN 2 HELP!');
+        }
+      }, 1000);
+    });
   }
 
 }
